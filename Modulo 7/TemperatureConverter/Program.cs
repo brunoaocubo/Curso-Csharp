f0d4ade0e4cc0f54daa0a99f7e91816a4a1bd715
brunoaocubo <116372081+brunoaocubo@users.noreply.github.com>
2026-04-08 18:05:46 -0300

    Conversor de Temperatura
    Celsius > Fahrenheit
    Celsius > Kelvin

---
+++ Modulo 7/TemperatureConverter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperatureConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double celsius, far, kelvin;
+            Console.WriteLine("### CONVERSOR DE TEMPERATURA ###");
+            Console.WriteLine("Digite um valor de temperatura em celsius, apenas números...");
+            celsius = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nConvertendo temperatura para Fahrenheit...");
+            far = (celsius * 9 / 5) + 32;
+            Console.WriteLine($"Temperatura em Fahrenheit: {far}ºF");
+
+            Console.WriteLine("\nConvertendo temperatura para Kelvin...");
+            kelvin = celsius + 273.15;
+            Console.WriteLine($"Temperatura em Kelvin: {kelvin} K");
+        }
+    }
+}
