a44cd55b1d570980c96997019d1911c260e838f9
brunoaocubo <116372081+brunoaocubo@users.noreply.github.com>
2026-04-09 18:02:19 -0300

    Modulo 8: Coleções de Array
    - Estrutura de Arrays
    - Matrizes
    - Multiplicação de Matrizes

---
+++ Modulo 8/Array/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region VETORES
+            /*
+            int[] numbers = new int[5];
+
+            numbers[0] = 2;
+            numbers[1] = 3;
+            numbers[2] = 4;
+            numbers[3] = 5;
+            numbers[4] = 6;
+
+            string[] names = new string[2];
+
+            names[0] = "Bruno";
+            names[1] = "Rodrigo";
+
+            string[] countries =
+            {
+                "EUA",
+                "BRA",
+                "CH"
+            };
+
+            Console.WriteLine(numbers[2]);
+            Console.WriteLine(names[1]);
+            Console.WriteLine(countries[1]);
+            */
+            #endregion
+
+            #region VETORES BIDIMENSIONAIS
+
+            
+            int[,] numbers2 = new int[3, 3];
+
+            numbers2[0, 0] = 10;
+            numbers2[0, 1] = 20;
+            numbers2[0, 2] = 30;
+
+            numbers2[1, 0] = 40;
+            numbers2[1, 1] = 50;
+            numbers2[1, 2] = 60;
+
+            double[,] mydouble =
+            {
+                {80, 100},
+                {120, 140 }
+            };
+
+            Console.WriteLine(numbers2[1, 2]);
+            Console.WriteLine(mydouble[0,1]);
+            #endregion
+        }
+    }
+}
---
+++ Modulo 8/MatrixMultiplication/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixMultiplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix1 = new int[2, 3];
+            int[,] matrix2 = new int[3, 2];
+            int[,] resultado = new int[2,2];
+
+            Console.WriteLine("### Preencha a primeira matriz ###");
+
+            for (int row = 0; row < 2; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    Console.WriteLine($"\n# Posição[{row}][{col}]:");
+                    matrix1[row, col] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("### Preencha a segunda matriz ###");
+
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 2; col++)
+                {
+                    Console.WriteLine($"#Posição[{row}][{col}]:");
+                    matrix2[row, col] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("### Resultado Matriz#1 x Matriz#2: ###");
+            resultado[0, 0] =
+                (matrix1[0,0] * matrix2[0,0]) +
+                (matrix1[0,1] * matrix2[1,0]) +
+                (matrix1[0,2] * matrix2[2,0]);
+            
+            resultado[1, 0] =(matrix1[1,0] * matrix2[0,0]) + (matrix1[1,1] * matrix2[1,0]) + (matrix1[1,2] * matrix2[2,0]);
+            
+            resultado[0,1] = (matrix1[0,0] * matrix2[0,1]) + (matrix1[0,1] * matrix2[1,1]) + (matrix1[0,2] * matrix2[2,1]);
+            
+            resultado[1,1] = (matrix1[1,0] * matrix2[0,1]) + (matrix1[1,1] * matrix2[1,1]) + (matrix1[1,2] * matrix2[2,1]);
+
+            Console.WriteLine($"[{resultado[0, 0]}][{resultado[0, 1]}]");
+            Console.WriteLine($"[{resultado[1, 0]}][{resultado[1,1]}]");
+
+        }
+    }
+}
