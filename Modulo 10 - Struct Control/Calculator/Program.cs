ed15e4c2d9f13367fb54f36825d3c001b7d954f8
brunoaocubo <116372081+brunoaocubo@users.noreply.github.com>
2026-04-09 18:03:45 -0300

    Modulo 10 - Estruturas de Controle
    - SWITCH/CASE
    - GOTO
    - Calculadora

---
+++ Modulo 10 - Struct Control/Calculator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        enum options
+        {
+            sum = 1,
+            sub = 2,
+            multi = 3,
+            div = 4,
+            restdiv = 5,
+            potency = 6
+        }
+
+        static void Main(string[] args)
+        {
+            double sum, sub, multi, div, restdiv, potency;
+
+            INIT:
+            Console.WriteLine("### CALCULADORA! ###\n");
+            Console.WriteLine("Selecione uma das operações abaixo: ");
+            Console.WriteLine($"({((int)options.sum)})Adição\n({(int)options.sub})Subtração\n({(int)options.multi})Multiplicação\n({(int)options.div})Divisão\n({(int)options.restdiv})Resto da Divisão\n({(int)options.potency})Potenciação\n");
+            
+            int option = int.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                default:
+                    goto INIT;
+                case 1:
+
+                    break;
+            }
+
+        }
+    }
+}
---
+++ Modulo 10 - Struct Control/Switch/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region SWITCH/CASE
+            int value = 0;
+
+            switch (value)
+            {
+                case 10:
+                    Console.WriteLine("O valor está correto");
+                    break;
+                case 20:
+                    Console.WriteLine("O valor tem uma diferença de 10");
+                    break;
+                case 30:
+                    Console.WriteLine("O valor tem uma diferença de 20");
+                    break;
+                default:
+                    Console.WriteLine("Nenhum valor alvo foi identificado.");
+                    break;
+            }
+            #endregion
+
+            #region GOTO
+            
+            START:
+            Console.WriteLine("Selecione uma opção, (1) (2): ");
+            int option = int.Parse(Console.ReadLine());
+            int result = 0;
+
+            switch (option)
+            {
+                default:
+                    goto START;
+                case 1:
+                    result += 100;
+                    break;
+                case 2:
+                    result += 50;
+                    goto case 1;
+            }
+
+            Console.WriteLine($"O valor total é: {result}");
+            #endregion
+        }
+    }
+}
