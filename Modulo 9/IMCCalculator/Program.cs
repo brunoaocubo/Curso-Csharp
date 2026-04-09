b59791da3119be8e255308e4f822f756a9034192
brunoaocubo <116372081+brunoaocubo@users.noreply.github.com>
2026-04-09 18:03:13 -0300

    Modulo 9: Condicionais - Calculadora IMC
    -Estruturas Condicionais Simples
    -Estruturas Condicionais Aninhadas

---
+++ Modulo 9/IMCCalculator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMCCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double weight, height;
+            Console.WriteLine("### CALULADORA IMC ###\n");
+            Console.WriteLine("### Informe o seu peso: ###\n");
+            weight = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("### Informe a sua altura: ###\n");
+            height = double.Parse(Console.ReadLine());
+
+            double resultado = weight / (height * height);
+            string resposta;
+
+            if (resultado < 18.5) { resposta = "Magreza"; }
+            else if (resultado >= 18.5 || resultado <= 24.9) { resposta = "Peso Normal"; }
+            else if (resultado >= 25 || resultado <= 29.9) { resposta = "Acima do Peso"; }
+            else{
+                resposta = "Obesidade"; 
+            }
+
+            Console.WriteLine($"Valor cálculo IMC: {resultado}\nResultado: {resposta}");
+        }
+    }
+}
