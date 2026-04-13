4b3c852981be7c79009611f17a32289a613cb36f
brunoaocubo <116372081+brunoaocubo@users.noreply.github.com>
2026-04-13 07:48:28 -0300

    Modulo 12 - Delegates

---
+++ Modulo 12 - OOP/Delegates/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    internal class Program
+    {
+        delegate void Operation(int num1, int num2);
+
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            Operation delegateOp = null;
+
+            delegateOp += math.Sum; //Inscrição do delegate para utilizar o metodo Sum() de Math.
+            delegateOp += math.Sub; //Mais uma atribuição ao delegate para ser utilizado.
+            delegateOp += math.Multi;
+            delegateOp += math.Div;
+
+            delegateOp(20, 5); //Uma chamada, quatro respostas com o mesmo valor.
+        }
+    }
+}
