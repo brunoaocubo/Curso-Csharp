86413331c3501aa9d1dcf8c7fb89c37d796176ce
brunoaocubo <116372081+brunoaocubo@users.noreply.github.com>
2026-04-13 07:48:07 -0300

    Modulo 12 - Metodo This

---
+++ Modulo 12 - OOP/ThisMethod/Acess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThisMethod
+{
+    internal class Acess
+    {
+        string password = "123";
+
+        public bool Login(string password)
+        {
+            return this.password == password;
+        }
+    }
+}
---
+++ Modulo 12 - OOP/ThisMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThisMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Acess acess = new Acess();
+
+            if (acess.Login("123"))
+            {
+                Console.WriteLine("Login com sucesso!");
+            }
+            else
+            {
+                Console.WriteLine("Falha no login: Senha Incorreta!");
+            }
+        }
+    }
+}
