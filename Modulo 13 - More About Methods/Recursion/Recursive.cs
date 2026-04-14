7d226cf38910dcfbc43d863282f7cbb219f4f0a4
brunoaocubo <116372081+brunoaocubo@users.noreply.github.com>
2026-04-14 16:33:02 -0300

    Modulo 13 - Recursividade
    - Método recursivo

---
+++ Modulo 13 - More About Methods/Recursion/Recursive.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion
+{
+    internal class Recursive
+    {
+        public void ExecuteNonRecursive(string message, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(message);
+            }
+        }
+
+        public void ExecuteRecursive(string message, int n)
+        {
+            if(n > 0)
+            {
+                Console.WriteLine(message);
+                ExecuteRecursive(message, n - 1);
+            }
+        }
+    }
+}
