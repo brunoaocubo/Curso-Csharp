45a0a747b02321d1ce502057da36f93e29c5652b
brunoaocubo <116372081+brunoaocubo@users.noreply.github.com>
2026-04-14 08:34:05 -0300

    Módulo 12 - Encapsulamento

---
+++ Modulo 12 - OOP/Encapsulate/BankAccount.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulate
+{
+    internal class BankAccount
+    {
+        private string _client;
+        private double _balance;
+        private double _taxeCashOut = 15;
+
+        public string Client { get { return _client; }
+            set
+            {
+                if(_client == null || _client == "")
+                {
+                    _client = value;
+                }
+            }
+        }
+
+        public void CheckBalance()
+        {
+            Console.WriteLine($"Nome:{Client}\nSaldo:{_balance}");
+        }
+
+        public void CashOut(double value)
+        {
+            value += _taxeCashOut;
+
+            if(_balance >= value)
+            {
+                _balance -= value;
+            }
+        }
+
+        public void Deposit(double value)
+        {
+            _balance += value;
+        }
+    }
+}
---
+++ Modulo 12 - OOP/Encapsulate/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount client1 = new BankAccount();
+
+            client1.Client = "Tulia";
+            client1.Client = "Jasmine";
+            client1.Deposit(50);
+            client1.CheckBalance();
+
+        }
+    }
+}
