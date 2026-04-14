560114e17a8cd5d84d21275934f42389a8332951
brunoaocubo <116372081+brunoaocubo@users.noreply.github.com>
2026-04-14 10:49:56 -0300

    Modulo 12 - Modificadores de Acesso
    - Private / Internal

---
+++ Modulo 12 - OOP/Modifiers/Classes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modifiers
+{
+    internal class Classes
+    {
+        private string name;
+        public string lastName;
+
+        private void Method1() { }
+        public void Method2() { }
+
+    }
+
+    class Human
+    {
+        public string name;
+        protected string lastName;
+        internal int idade; // Restringe para dentro do nameespace "Modifiers".
+    }
+
+    class Man : Human
+    {
+        public void Method1()
+        {
+            lastName = "Variavel com Protected";
+            idade = 0;
+        }
+    }
+}
---
+++ Modulo 12 - OOP/Modifiers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modifiers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Classes teste = new Classes();
+            //teste.name = ; Modficador Private
+            teste.lastName = ""; 
+            //teste.Method1(); Modficador Private
+            teste.Method2();
+        }
+    }
+}
