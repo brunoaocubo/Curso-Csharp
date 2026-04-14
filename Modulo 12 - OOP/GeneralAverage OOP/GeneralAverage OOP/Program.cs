6f42dbce6dd50907406d70e9bf3d8aadf4dddbd5
brunoaocubo <116372081+brunoaocubo@users.noreply.github.com>
2026-04-14 16:04:38 -0300

    Modulo 12 - Média Geral POO

---
+++ Modulo 12 - OOP/GeneralAverage OOP/GeneralAverage OOP/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralAverage_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("### MÉDIA GERAL DOS ALUNOS ###");
+            Console.Write("\nQuantos alunos?: ");
+            int qttStudents = int.Parse(Console.ReadLine());
+            Student[] students = new Student[qttStudents];
+            Console.Clear();
+
+            for (int i = 0; i < students.Length; i++)
+            {
+            INIT:
+                Console.Clear();
+                try
+                {
+                    Console.Write($"\nNome do Aluno #{i + 1}: ");
+                    string name = Console.ReadLine();
+                    Console.Write("\nQuantidade de Provas: ");
+                    int qttExams = int.Parse(Console.ReadLine());
+                    Console.Clear();
+                    students[i] = new Student(name, qttExams);
+                    students[i].InsertNotes();
+                    //Console.WriteLine($"\nMedia de notas do aluno, {students[i].Name}: {Math.Round(students[i].Average, 2)}");
+                }
+                catch
+                {
+                    goto INIT;
+                }  
+            }
+
+            double generalAverage = 0;
+
+            foreach (Student item in students)
+            {
+                generalAverage += item.Average;
+                Console.WriteLine($"\nAluno: {item.Name}\nMédia: {item.Average}");
+            }
+            
+
+            Console.WriteLine($"\nMédia Geral dos Alunos: {Math.Round((generalAverage / students.Length), 2)}");
+        }
+    }
+}
---
+++ Modulo 12 - OOP/GeneralAverage OOP/GeneralAverage OOP/Student.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralAverage_OOP
+{
+    internal class Student
+    {
+        public string Name { get; private set; }
+        private double[] notes = new double[3];
+
+        public double Average
+        {
+            get
+            {
+                return CalcAverage();
+            }
+        }
+
+        public Student(string name, int exam)
+        {
+            Name = name;
+            notes = new double[exam];
+        }
+
+        public void InsertNotes()
+        {
+            for (int i = 0; i < notes.Length; i++)
+            {
+            INIT:
+                Console.Write($"Insira o valor da prova {i + 1}: ");
+                double note = double.Parse(Console.ReadLine());
+
+                if(note < 0 || note > 10)
+                {
+                    Console.WriteLine("ERRO: é permitido apenas valores entre 0 e 10\n");
+                    goto INIT;
+                }
+
+                notes[i] = note;
+
+                if (i >= notes.Length)
+                {
+                    GetNotes();
+                }
+            }
+        }
+
+        private double CalcAverage()
+        {
+            double sum = 0;
+
+            foreach (double item in notes)
+            {
+                sum += item;
+            }
+
+            return Math.Round((sum / notes.Length), 2);
+        }
+
+        public void GetNotes()
+        {
+            Console.WriteLine($"Quantidade de Provas: {notes.Length}");
+
+            foreach (var item in notes)
+            {
+                Console.WriteLine($"Nota: {item}");
+            }
+        }
+    }
+}
