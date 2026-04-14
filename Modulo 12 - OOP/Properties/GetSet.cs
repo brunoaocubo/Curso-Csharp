ed264695de754946997a2c1423c46163b32be6c6
brunoaocubo <116372081+brunoaocubo@users.noreply.github.com>
2026-04-14 08:32:40 -0300

    Módulo 12 - Propriedades da classe GET/SET

---
+++ Modulo 12 - OOP/Properties/GetSet.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    internal class GetSet
+    {
+        private string _name;
+        public string LastName { get; } = "Brennford";
+        private int _age;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if (value > 18)
+                {
+                    _age = value;
+                }
+                else
+                {
+                    Console.WriteLine("Idade majoritaria inferior a 18 anos.");
+                }
+            }
+        }
+    }
+}
---
+++ Modulo 12 - OOP/Properties/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GetSet person = new GetSet();
+
+            person.Name = "Sophia";
+            person.Age = 17;
+
+            Console.WriteLine(person.Name + " " + person.LastName);
+            Console.WriteLine(person.Age);
+        }
+    }
+}
