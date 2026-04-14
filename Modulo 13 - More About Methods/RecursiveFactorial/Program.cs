6f88532f2053cfca5940ac19eee1ce1b18a7b329
brunoaocubo <116372081+brunoaocubo@users.noreply.github.com>
2026-04-14 18:25:07 -0300

    Modulo 13 - Fatorial Recursivo

---
+++ Modulo 13 - More About Methods/RecursiveFactorial/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursiveFactorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número para descobrir o fatorial: ");
+            int numFactor = int.Parse(Console.ReadLine());
+            Console.WriteLine($"\nO fatorial de {numFactor} é: {Factorial(numFactor)}");
+        }
+
+        static int Factorial(int num)
+        {
+            if(num <= 0)
+            {
+                return 1;
+            }
+
+            return num * Factorial(num - 1);
+        }
+    }
+}
