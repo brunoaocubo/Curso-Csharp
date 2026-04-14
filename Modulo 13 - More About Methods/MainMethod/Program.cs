5b62def78af0c004064bef284f0fd751b408778e
brunoaocubo <116372081+brunoaocubo@users.noreply.github.com>
2026-04-14 17:24:35 -0300

    Modulo 13 - MainMethod - Argumentos
    - Definindo valor para o ARGS do Main para limitar o acesso a aplicação.

---
+++ Modulo 13 - More About Methods/MainMethod/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    internal class Program
+    {
+        //Utilizando ARGS do Main para limitar o acesso a aplicação, sendo necessário abrir pelo cmd e colocar os argumentos de entrada.
+        static void Main(string[] args)
+        {
+            string password = "s123";
+
+            if (args[1] != password)
+            {
+                Console.WriteLine("Acesso Negado");
+            }
+            else
+            {
+                Console.WriteLine($"Olá {args[0]}");
+            }
+        }
+    }
+}
