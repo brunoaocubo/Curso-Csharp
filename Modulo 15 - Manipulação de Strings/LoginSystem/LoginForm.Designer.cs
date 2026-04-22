425e187efcb6e74fe55b0c5d20f165906fd3618c
brunoaocubo <116372081+brunoaocubo@users.noreply.github.com>
2026-04-22 17:36:25 -0300

    Sistema de Login - Page Login

---
+++ Modulo 15 - Manipulação de Strings/LoginSystem/LoginForm.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace LoginSystem
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.userInput = new System.Windows.Forms.TextBox();
+            this.passwordInput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label1.Location = new System.Drawing.Point(13, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuário:";
+            // 
+            // userInput
+            // 
+            this.userInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.userInput.Location = new System.Drawing.Point(17, 116);
+            this.userInput.Multiline = true;
+            this.userInput.Name = "userInput";
+            this.userInput.Size = new System.Drawing.Size(270, 26);
+            this.userInput.TabIndex = 1;
+            this.userInput.WordWrap = false;
+            // 
+            // passwordInput
+            // 
+            this.passwordInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.passwordInput.Location = new System.Drawing.Point(17, 181);
+            this.passwordInput.Multiline = true;
+            this.passwordInput.Name = "passwordInput";
+            this.passwordInput.PasswordChar = '*';
+            this.passwordInput.Size = new System.Drawing.Size(270, 26);
+            this.passwordInput.TabIndex = 3;
+            this.passwordInput.WordWrap = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label2.Location = new System.Drawing.Point(13, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Senha:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 229);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 60);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Entrar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(178, 229);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 60);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Limpar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label3.Location = new System.Drawing.Point(111, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 31);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Login";
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(301, 304);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.passwordInput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.userInput);
+            this.Controls.Add(this.label1);
+            this.Name = "LoginForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox userInput;
+        private System.Windows.Forms.TextBox passwordInput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
