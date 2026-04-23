9f6595295bf2168a23d239a8ba5b9f7619974d4e
brunoaocubo <116372081+brunoaocubo@users.noreply.github.com>
2026-04-23 16:10:06 -0300

    Modulo 16 - To String
    - Número hexadecimal, com valor minimo de casas;
    - Número em decimal, com valor minimo de casas;
    - Monetário

--- Modulo 16 - NumbersManipulation/Modulo 16 - NumbersManipulation/Form1.cs
+++ Modulo 16 - NumbersManipulation/Modulo 16 - NumbersManipulation/Form1.cs
@@ -34,6 +34,23 @@ namespace Modulo_16___NumbersManipulation
             }
             */
             #endregion
+
+            #region ToString
+            /*
+            double num = 19.96854d;
+            //Formato de string para escolher o número de casas.
+            txt_result.Text += num.ToString("#.00 \n");
+
+            int myInt = 10;
+
+            txt_result.Text += myInt.ToString("X2 \n"); // Número em hexadecimal, com minimo de 2 digitos.
+            txt_result.Text += myInt.ToString("D2 \n"); // Número em decimal, com minimo de 2 digitos.
+            txt_result.Text += myInt.ToString("X \n");  // Número em hexadecimal.
+            txt_result.Text += myInt.ToString("D \n");  // Número em decimal.
+            txt_result.Text += myInt.ToString("C \n"); // Número em Monetário.
+            txt_result.Text += Convert.ToString(myInt, 8);
+            */
+            #endregion
         }
     }
 }
