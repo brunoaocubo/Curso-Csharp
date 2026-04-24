6778482c18ee074b4538f37e4bd773767f6f85ac
brunoaocubo <116372081+brunoaocubo@users.noreply.github.com>
2026-04-24 14:01:30 -0300

    Modulo 17 - Coleções Genéricas - List()

---
+++ Modulo 17 - AdvancedCollections/GenericsCollecctions/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace GenericsCollecctions
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnList = new System.Windows.Forms.Button();
+            this.listResult = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnList
+            // 
+            this.btnList.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.btnList.Location = new System.Drawing.Point(641, 12);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(147, 42);
+            this.btnList.TabIndex = 0;
+            this.btnList.Text = "List";
+            this.btnList.UseVisualStyleBackColor = true;
+            this.btnList.Click += new System.EventHandler(this.btnList_Click);
+            // 
+            // listResult
+            // 
+            this.listResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.listResult.FormattingEnabled = true;
+            this.listResult.ItemHeight = 20;
+            this.listResult.Location = new System.Drawing.Point(13, 12);
+            this.listResult.Name = "listResult";
+            this.listResult.Size = new System.Drawing.Size(622, 424);
+            this.listResult.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listResult);
+            this.Controls.Add(this.btnList);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnList;
+        private System.Windows.Forms.ListBox listResult;
+    }
+}
+
---
+++ Modulo 17 - AdvancedCollections/GenericsCollecctions/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GenericsCollecctions
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            listResult.Items.Clear();
+
+            //Array
+            string[] namesArr = new string[3];
+            namesArr[0] = "Octavia";
+            namesArr[1] = "Paula";
+            namesArr[2] = "Pedro";
+
+            List<string> namesList = new List<string>()
+            {
+                "Gustavo",
+                "Samantha"
+            };
+            namesList.AddRange(namesArr);
+
+            /*
+            if (namesList.Remove("Pedro"))
+            {
+                MessageBox.Show("Pedro removido");
+            }
+            else
+            {
+                MessageBox.Show("Não foi possível remover");
+            }*/
+            
+            namesList.Sort();
+
+            MessageBox.Show(namesList.IndexOf("Paula").ToString());
+
+            namesList.Insert(2, "Sofia");
+
+            namesList.RemoveAt(0);
+
+            foreach (string item in namesList)
+            {
+                listResult.Items.Add(item);
+            }
+
+        }
+    }
+}
