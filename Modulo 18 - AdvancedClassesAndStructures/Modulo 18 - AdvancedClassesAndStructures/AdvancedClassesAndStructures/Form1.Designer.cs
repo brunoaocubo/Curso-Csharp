5329c02027eeaba790084b03172aa945081462e7
brunoaocubo <116372081+brunoaocubo@users.noreply.github.com>
2026-04-30 13:10:17 -0300

    Modulo 18 - Classes e Estruturas Avançadas - Random
    - Geração de números aleatórios.
    - Utilização de Seeds para definir um ponto de partida diferente.

---
+++ Modulo 18 - AdvancedClassesAndStructures/Modulo 18 - AdvancedClassesAndStructures/AdvancedClassesAndStructures/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace AdvancedClassesAndStructures
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtResult = new System.Windows.Forms.Label();
+            this.btnRandom = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtResult
+            // 
+            this.txtResult.AutoSize = true;
+            this.txtResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.txtResult.Location = new System.Drawing.Point(8, 9);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(86, 31);
+            this.txtResult.TabIndex = 0;
+            this.txtResult.Text = "label1";
+            // 
+            // btnRandom
+            // 
+            this.btnRandom.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.btnRandom.Location = new System.Drawing.Point(12, 333);
+            this.btnRandom.Name = "btnRandom";
+            this.btnRandom.Size = new System.Drawing.Size(121, 55);
+            this.btnRandom.TabIndex = 1;
+            this.btnRandom.Text = "Random";
+            this.btnRandom.UseVisualStyleBackColor = true;
+            this.btnRandom.Click += new System.EventHandler(this.btnRandom_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRandom);
+            this.Controls.Add(this.txtResult);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtResult;
+        private System.Windows.Forms.Button btnRandom;
+    }
+}
+
---
+++ Modulo 18 - AdvancedClassesAndStructures/Modulo 18 - AdvancedClassesAndStructures/AdvancedClassesAndStructures/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdvancedClassesAndStructures
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRandom_Click(object sender, EventArgs e)
+        {
+            //Utilizando seed para ter um ponto de partida diferente.
+            Random random = new Random(DateTime.Now.Millisecond);
+            int myInt = random.Next(100); //Limitar
+
+            //Gerar valores flutuantes multicados por 100.
+            double myDouble = random.NextDouble() * 100;
+
+            txtResult.Text = $"Número gerado com Random(): {myDouble}";
+
+        }
+    }
+}
