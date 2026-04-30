5c53ed17272b2eae1b67c82d448e555c05a08afc
brunoaocubo <116372081+brunoaocubo@users.noreply.github.com>
2026-04-30 16:52:54 -0300

    Modulo 18 - Thread Console
    - Metodos base .
    - Exemplos claros de funcionamento da thread com a thread principal (Main()).

---
+++ Modulo 18 - AdvancedClassesAndStructures/ThreadConsole/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(new ThreadStart(ExecuteTask));
+            thread.IsBackground = true; // Encerra a thread se a thread principal estiver finalizado.
+            thread.Join(); //Bloqueia a thread principal, Main(), até ExecuteTask() esteja finalizado.
+            thread.Start();
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Principal Thread");
+                Thread.Sleep(500);
+            }
+
+            Console.ReadKey();
+        }
+
+        static void ExecuteTask()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Tarefa executada.");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
