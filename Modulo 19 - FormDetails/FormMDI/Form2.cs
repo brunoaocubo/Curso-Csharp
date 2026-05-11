8dc9f7ab831e68b6e8270ba6a1660e0a8c9894d6
brunoaocubo <116372081+brunoaocubo@users.noreply.github.com>
2026-05-11 12:17:50 -0300

    Modulo 19 - MDI Container
    - Criação de um container de formulários.

---
+++ Modulo 19 - FormDetails/FormMDI/Form2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormMDI
+{
+    public partial class Form2 : Form
+    {
+        public Form2(Form parent)
+        {
+            InitializeComponent();
+
+            MdiParent = parent;
+        }
+
+        private void btnForm3_Click(object sender, EventArgs e)
+        {
+            Form3 form3 = new Form3(MdiParent);
+            form3.Show();
+            Close();
+        }
+    }
+}
---
+++ Modulo 19 - FormDetails/FormMDI/Form3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormMDI
+{
+    public partial class Form3 : Form
+    {
+        public Form3(Form parent)
+        {
+            InitializeComponent();
+
+            MdiParent = parent;
+        }
+
+        private void btnForm2_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2(MdiParent);
+            form2.Show();
+            Close();
+        }
+    }
+}
