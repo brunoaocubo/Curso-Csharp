b5d603e6ab3ef85d74782ae9b5da1cb002278195
brunoaocubo <116372081+brunoaocubo@users.noreply.github.com>
2026-05-13 17:47:50 -0300

    Modulo 20 - 015 Progress Bar

---
+++ Modulo 20 - FormControl Details/015/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace _015
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtProgress = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(171, 123);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(456, 54);
+            this.progressBar.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(317, 343);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(182, 62);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = " Iniciar!";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtProgress
+            // 
+            this.txtProgress.AutoSize = true;
+            this.txtProgress.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtProgress.Location = new System.Drawing.Point(376, 195);
+            this.txtProgress.Name = "txtProgress";
+            this.txtProgress.Size = new System.Drawing.Size(38, 22);
+            this.txtProgress.TabIndex = 2;
+            this.txtProgress.Text = "0%";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtProgress);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.progressBar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label txtProgress;
+    }
+}
+
---
+++ Modulo 20 - FormControl Details/015/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _015
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+            List<Task> tasks = new List<Task>();
+
+            tasks.Add(new Task(new Action(() =>
+            {
+                Thread.Sleep(2000);
+                progressBar.Invoke(new Action(() =>
+                {
+                    progressBar.Value ++;
+                }));
+            })));
+
+            tasks.Add(new Task(new Action(() =>
+            {
+                Thread.Sleep(2000);
+                progressBar.Invoke(new Action(() =>
+                {
+                    progressBar.PerformStep();
+                }));
+            })));
+
+            tasks.Add(new Task(new Action(() =>
+            {
+                Thread.Sleep(2000);
+                progressBar.Invoke(new Action(() =>
+                {
+                    progressBar.PerformStep();
+                }));
+            })));
+
+            tasks.Add(new Task(new Action(() =>
+            {
+                Thread.Sleep(2000);
+                progressBar.Invoke(new Action(() =>
+                {
+                    progressBar.PerformStep();
+                }));
+            })));
+
+            progressBar.Maximum = tasks.Count;
+            progressBar.Step = 1;
+
+            for (int i = 0; i < tasks.Count; i++)
+            {
+                tasks[i].Start();
+            }
+        }
+    }
+}
