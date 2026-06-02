6c3c961dc0fac4be2a8f695e24078d2a77136bfd
brunoaocubo <116372081+brunoaocubo@users.noreply.github.com>
2026-06-02 10:58:35 -0300

    Modulo 21 - Tratamento de Erros
    Uso do try/catch para tratamento de erro.

---
+++ Modulo 21 - Error Handling/TryCatch/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TryCatch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int[] numbers = { 10, 20, 30, 40, 50 };
+                string s = "";
+                for (int i = 0; i < 6; i++)
+                {
+                    s += numbers[i] + " ";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                label1.Text = "Erro:" + ex.Message;
+            }
+        }
+    }
+}
