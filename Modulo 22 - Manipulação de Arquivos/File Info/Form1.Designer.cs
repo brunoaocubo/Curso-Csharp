b5ba4952a607520169156bf92914e2ccc4f8cb11
brunoaocubo <116372081+brunoaocubo@users.noreply.github.com>
2026-07-08 09:32:56 -0300

    Modulo 22 - FileInfo
    Classe para obter as informações de um arquivo tais quais:
    FullName;
    Name;
    Directory;
    CreationTime;
    Extension;
    LastTime.

---
+++ Modulo 22 - Manipulação de Arquivos/File Info/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace File_Info
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.list = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // list
+            // 
+            this.list.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.list.FormattingEnabled = true;
+            this.list.ItemHeight = 20;
+            this.list.Location = new System.Drawing.Point(12, 12);
+            this.list.Name = "list";
+            this.list.Size = new System.Drawing.Size(1040, 404);
+            this.list.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.button1.Location = new System.Drawing.Point(321, 440);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(400, 106);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Executar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1074, 579);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.list);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox list;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Modulo 22 - Manipulação de Arquivos/File Info/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace File_Info
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string folder = @"C:\Users\bruno.carvalho\Desktop\";
+            string file = "Teste.txt";
+            string path_file = folder + file;
+
+            FileInfo info = new FileInfo(path_file);
+
+            if (!info.Exists)
+            {
+                File.Create(folder + file).Close();   
+            }
+
+            list.Items.Clear();
+            list.Items.Add(info.FullName);
+            list.Items.Add(info.Name);
+            list.Items.Add(info.Directory.FullName);
+            list.Items.Add(info.DirectoryName);
+            list.Items.Add(info.CreationTime);
+            list.Items.Add(info.Extension);
+            list.Items.Add(info.LastAccessTime);
+        }
+    }
+}
