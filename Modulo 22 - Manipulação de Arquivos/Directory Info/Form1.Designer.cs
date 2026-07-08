af58d639c36bb6eb1a8c2c580b13cc75db549936
brunoaocubo <116372081+brunoaocubo@users.noreply.github.com>
2026-07-08 12:08:01 -0300

    Modulo 22 - Directory Info

---
+++ Modulo 22 - Manipulação de Arquivos/Directory Info/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Directory_Info
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.list = new System.Windows.Forms.ListBox();
+            this.Execute = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // list
+            // 
+            this.list.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.list.FormattingEnabled = true;
+            this.list.ItemHeight = 24;
+            this.list.Location = new System.Drawing.Point(13, 13);
+            this.list.Name = "list";
+            this.list.Size = new System.Drawing.Size(775, 340);
+            this.list.TabIndex = 0;
+            // 
+            // Execute
+            // 
+            this.Execute.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.Execute.Location = new System.Drawing.Point(263, 374);
+            this.Execute.Name = "Execute";
+            this.Execute.Size = new System.Drawing.Size(260, 64);
+            this.Execute.TabIndex = 1;
+            this.Execute.Text = "Executar";
+            this.Execute.UseVisualStyleBackColor = true;
+            this.Execute.Click += new System.EventHandler(this.Execute_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Execute);
+            this.Controls.Add(this.list);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox list;
+        private System.Windows.Forms.Button Execute;
+    }
+}
+
---
+++ Modulo 22 - Manipulação de Arquivos/Directory Info/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Directory_Info
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Execute_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(@"C:\Users\bruno.carvalho\Desktop");
+
+            list.Items.Clear();
+            list.Items.Add(dirInfo.FullName);
+            list.Items.Add(dirInfo.Parent);
+            list.Items.Add(dirInfo.Name);
+            list.Items.Add(dirInfo.CreationTime);
+            list.Items.Add(dirInfo.Exists);
+            list.Items.Add(dirInfo.Root);
+            list.Items.Add("------------------");
+
+            DirectoryInfo[] dirs = dirInfo.GetDirectories();
+            foreach (var item in dirs)
+            {
+                list.Items.Add(item.FullName);
+            }
+            list.Items.Add("------------------");
+
+            FileInfo[] files = dirInfo.GetFiles();
+            foreach (var item in files)
+            {
+                list.Items.Add(item.Name);
+            }
+        }
+    }
+}
