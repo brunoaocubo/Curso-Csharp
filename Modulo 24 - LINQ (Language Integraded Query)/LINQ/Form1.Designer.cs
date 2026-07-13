13f82360aafefb87dc3206ee3a83b4ffb9f47589
brunoaocubo <116372081+brunoaocubo@users.noreply.github.com>
2026-07-13 11:50:05 -0300

    Modulo 24 - Introdução ao LINQ
    Estrutura básica do linq:
    
    IEnumerable<string> res_names = from name in list_names
                              where name.StartsWith(inputTxt.Text)
                              select name;
    
    //Opção 1
    listResult.Items.AddRange(res_names.ToArray());
    
    //Opção2: O mesmo foreach é utilizado, mas dessa vez é com a coleção ja com os valores encontrados.
    /*
    foreach (string name in res_names)
    {
        if (name.StartsWith(inputTxt.Text))
        {
            listResult.Items.Add(name);
        }
    }
    */

---
+++ Modulo 24 - LINQ (Language Integraded Query)/LINQ/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace LINQ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listResult = new System.Windows.Forms.ListBox();
+            this.inputTxt = new System.Windows.Forms.TextBox();
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listResult
+            // 
+            this.listResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.listResult.FormattingEnabled = true;
+            this.listResult.ItemHeight = 29;
+            this.listResult.Location = new System.Drawing.Point(13, 13);
+            this.listResult.Name = "listResult";
+            this.listResult.Size = new System.Drawing.Size(598, 410);
+            this.listResult.TabIndex = 0;
+            // 
+            // inputTxt
+            // 
+            this.inputTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.inputTxt.Location = new System.Drawing.Point(617, 13);
+            this.inputTxt.Multiline = true;
+            this.inputTxt.Name = "inputTxt";
+            this.inputTxt.Size = new System.Drawing.Size(171, 39);
+            this.inputTxt.TabIndex = 1;
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.btnExecute.Location = new System.Drawing.Point(617, 58);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(171, 41);
+            this.btnExecute.TabIndex = 2;
+            this.btnExecute.Text = "Executar";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExecute);
+            this.Controls.Add(this.inputTxt);
+            this.Controls.Add(this.listResult);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listResult;
+        private System.Windows.Forms.TextBox inputTxt;
+        private System.Windows.Forms.Button btnExecute;
+    }
+}
+
---
+++ Modulo 24 - LINQ (Language Integraded Query)/LINQ/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LINQ
+{
+    public partial class Form1 : Form
+    {
+        List<string> list_names = new List<string>()
+        {
+            "Loren", "Carla", "Jessi", "Alfredo", "João", "Lucas", "Anabel", "Cassia"
+        };
+
+        List<int> list_numbers = new List<int>()
+        {
+            1,2,3,4,5,6,7,9,10,11,12
+        };
+
+        Dictionary<string, double> list_products = new Dictionary<string, double>()
+        {
+            {"Carregador USB", 59.99 }, {"Adaptador WiFi", 47.99}, {"Mouse Logitech M208", 87.59}, {"Microfone Acer L10", 158.99}, {"Fone Bluetooh Anker P20", 159.99}
+        };
+
+        Dictionary<string, string> list_states = new Dictionary<string, string>()
+        {
+            {"Rio de Janeiro", "Brasil"}, {"Atlanta", "EUA"}, {"Buenos Aires", "Argentina"}, {"Sidney", "Austrália"}, {"Bahia", "Brasil"}, {"Brisbane", "Austrália"}
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            //Metodo Comum e custoso
+            /*
+            foreach (string name in list_names)
+            {
+                if (name.StartsWith(inputTxt.Text))
+                {
+                    listResult.Items.Add(name);
+                }
+            }*/
+
+            #region LINQ - List_Names
+            
+            IEnumerable<string> res_names = from name in list_names
+                                      where name.StartsWith(inputTxt.Text)
+                                      select name;
+            
+            //Opção 1
+            listResult.Items.AddRange(res_names.ToArray());
+            
+
+            //Opção2: O mesmo foreach é utilizado, mas dessa vez é com a coleção ja com os valores encontrados.
+            /*
+            foreach (string name in res_names)
+            {
+                if (name.StartsWith(inputTxt.Text))
+                {
+                    listResult.Items.Add(name);
+                }
+            }
+            */
+            #endregion
+
+            #region LINQ - List_Numbers
+            IEnumerable<int> res_numbers = from num in list_numbers where num % 2 == 0 select num;
+
+            //Opção 1
+            listResult.DataSource = res_numbers.ToArray();
+
+            //Opção 2
+            /*
+            foreach (int num in res_numbers)
+            {
+                listResult.Items.Add(num);
+            }
+            */
+            #endregion
+        }
+    }
+}
