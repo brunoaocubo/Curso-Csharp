52a027306c64c768461243af5734fd9b9b34e465
brunoaocubo <116372081+brunoaocubo@users.noreply.github.com>
2026-07-14 08:43:49 -0300

    Modulo 25 - Draw Lines - GDI+

--- Modulo 25 - GDI+/GDI+/Form1.cs
+++ Modulo 25 - GDI+/GDI+/Form1.cs
@@ -23,9 +23,39 @@ namespace GDI_
 
             Graphics artist = Graphics.FromImage(paper);
 
-            artist.Clear(Color.Blue);
-
+            artist.Clear(Color.White);
             picture.BackgroundImage = paper;
+
+            #region Draw Lines
+            Brush pencil = new SolidBrush(Color.Red);
+
+            Pen pen1 = new Pen(pencil, 5f);
+            Point point1 = new Point(200, 200);
+            Point point2 = new Point(600, 200);
+
+            Pen pen2 = new Pen(Color.Black, 5f);
+            Point point3 = new Point(600, 200);
+            Point point4 = new Point(600, 350);
+
+            //artist.DrawLine(pen1, point1, point2);
+            //artist.DrawLine(pen2, point3, point4);
+
+            Point[] points =
+            {
+                new Point(200, 200),
+                new Point(400, 200),
+
+                new Point(399, 200),
+                new Point(200, 300),
+
+                new Point(200, 100),
+                new Point(398, 200),
+
+            };
+            artist.DrawLines(pen1, points);
+
+            #endregion
+
 
             paper.Save(@"c:\Users\bruno.carvalho\Desktop\Teste\paper.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
         }
