7700c8fdd80390c00fa952e491562d6210c276b3
brunoaocubo <116372081+brunoaocubo@users.noreply.github.com>
2026-07-15 14:03:24 -0300

    Modulo 27 - Comunicação Client/Server
    OBS: Para testar é necessário abrir as duas solução SocketServer.csproj e SocketClient.csproj respectivamente iniciando-os para se conectar e enviar uma mensagem.

---
+++ Modulo 27 - Sockets Client-Server/SocketClient/SocketClient/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+           
+            try
+            {
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
+                socket.Connect(endPoint);
+
+                Console.WriteLine("Conectado ao servidor!");
+                Console.WriteLine("Insira uma informação para enviar...");
+
+                string info = Console.ReadLine();
+
+                byte[] infoSend = Encoding.Default.GetBytes(info);
+                socket.Send(infoSend, 0, info.Length, SocketFlags.None);
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Não foi possível conectar ao servidor...");
+            }
+
+            socket.Close();
+        }
+    }
+}
